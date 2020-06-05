c050ea25d2f12b5fb08fe7ff98dea788cb0e9758
Razielini <razielco@gmail.com>
2020-06-05 17:27:02 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,9 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
-import { Jumbo } from '../components'
-import { SEO } from "../components"
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+import { Jumbo, SEO, Products } from '../components'
 
 export const query = graphql`
-  query GET_DESCRIPTION {
+  query GET_DATA {
     allSite{
       edges {
         node {
@@ -14,20 +13,40 @@ export const query = graphql`
         }
       }
     }
+    allStripeSku {
+      edges {
+        node {
+          id
+          price
+          product {
+            name
+            metadata {
+              description
+              img
+              wear
+            }
+          }
+        }
+      }
+    }
   }
 `
 
-const IndexPage = ({ data }) => (
-  <>
-    <SEO title="Home" />
-    <Jumbo
-      description={data.allSite.edges[0].node.siteMetadata.description}
-    />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/gracias">Go to page 2</Link>
-  </>
-)
+const IndexPage = ({ data }) => {
+  console.log('data ::', data)
+  return (
+    <>
+      <SEO title="Home" />
+      <Jumbo
+        description={data.allSite.edges[0].node.siteMetadata.description}
+      />
+      <Products products={ data.allStripeSku.edges } />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <Link to="/gracias">Go to page 2</Link>
+    </>
+  )
+}
 
 export default IndexPage
