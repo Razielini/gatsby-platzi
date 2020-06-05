e1842ebeda177a86e6585c7223b69b5c4820f18b
Razielini <razielco@gmail.com>
2020-06-05 17:26:27 -0600

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -1,55 +1,41 @@
+import React, { useContext } from "react"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
 import { MenuItem, StyledHeader } from '../styles/components'
+import { CartContext } from '../context'
 
-const Header = ({ siteTitle }) => (
-  <StyledHeader>
-    <Link to='/'>
-      <img src='https://i.postimg.cc/6q3pg48v/Logo.png' alt='logo' />
-    </Link>
-    <nav>
-      <ul>
-        <MenuItem margin>
-          <Link to='/'>
-            Productos
-          </Link>
-        </MenuItem>
-        <MenuItem margin>
-          <a href='www.google.com'>
-            Platzi
-          </a>
-        </MenuItem>
-        <MenuItem>
-          <Link to='/cart'>
-            <span>
-              <img src='https://i.postimg.cc/L6wpMxLt/cart.png' alt='imagen' />
-            </span>
-          </Link>
-        </MenuItem>
-      </ul>
-    </nav>
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </StyledHeader>
-)
+const Header = ({ siteTitle }) => {
+  const { cart } = useContext(CartContext)
+  return (
+    <StyledHeader>
+      <Link to='/'>
+        <img src='https://i.postimg.cc/6q3pg48v/Logo.png' alt='logo' />
+      </Link>
+      <nav>
+        <ul>
+          <MenuItem margin>
+            <Link to='/'>
+              Productos
+            </Link>
+          </MenuItem>
+          <MenuItem margin>
+            <a href='www.google.com'>
+              Platzi
+            </a>
+          </MenuItem>
+          <MenuItem>
+            <Link to='/cart'>
+              <span>
+                <img src='https://i.postimg.cc/L6wpMxLt/cart.png' alt='imagen' />
+                { cart.length }
+              </span>
+            </Link>
+          </MenuItem>
+        </ul>
+      </nav>
+    </StyledHeader>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
